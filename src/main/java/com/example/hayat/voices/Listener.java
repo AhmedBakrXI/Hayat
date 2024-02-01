5485f45ce3bd1a1a7061edf9c0655fa90f124c9a
AhmedBakrXI <ahmedbakr2501@gmail.com>
2024-02-01 21:55:54 +0200

    Sphinx4

--- src/main/java/com/example/hayat/voices/Listener.java
+++ src/main/java/com/example/hayat/voices/Listener.java
@@ -1,4 +1,16 @@
 package com.example.hayat.voices;
 
+import edu.cmu.sphinx.api.*;
+
+import java.io.IOException;
+
 public class Listener {
+    public Listener() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module com.example.hayat {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.logging;
+    requires commons.math3;
+    requires java.desktop;
+    requires java.xml.bind;
+    requires java.prefs;
+
     opens com.example.hayat to javafx.fxml;
     exports com.example.hayat;
 }
